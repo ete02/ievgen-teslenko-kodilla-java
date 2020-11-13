68479da610b1add9c3e7d175681138ea6c5f96e8
Ievgen Teslenko <eugen.te11@gmail.com>
2020-11-13 15:45:42 +0100

    mowing project from repo

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -0,0 +1,34 @@
+package com.kodilla.testing;
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+public class TestingMain {
+    public static void main(String[] args) {
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        // Tests for calculator
+        System.out.println("Test pierwszy jednostkowy");
+        Calculator calculator=new Calculator();
+        int a=8;
+        int b=3;
+
+        if(calculator.add(a,b)==(a+b)){
+            System.out.println("Test is working");
+        }else{
+            System.out.println("test OK");
+        }
+        System.out.println("Error!");
+        if(calculator.subtract(a,b)==(a-b)){
+            System.out.println("test Ok");
+        }else{
+            System.out.println("Error!");
+        }
+
+    }
+}
