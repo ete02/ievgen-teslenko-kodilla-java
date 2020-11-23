a201454dbbf8a878d2529cdf72a36c8e9e4d1318
Ievgen Teslenko <eugen.te11@gmail.com>
2020-11-23 18:48:28 +0100

    Module7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,14 +1,42 @@
 package com.kodilla.stream;
+import com.kodilla.stream.lambda.ExecuteSaySomething;
+import com.kodilla.stream.lambda.Processor;
+import com.kodilla.stream.lambda.Executor;
 import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.beautifier.PoemDecorator;
 
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
 public class StreamMain<text> {
 
     public static void main(String[] args) {
-        final PoemBeautifier poemBeautifier = new PoemBeautifier();
+        /*final SaySomething saySomething = new SaySomething();
+        saySomething.say();*/
+        /*final Processor processor = new Processor();
+        final ExecuteSaySomething executeSaySomething = new ExecuteSaySomething();
+        processor.execute(executeSaySomething);*/
+
+        /*fianl Processor processor = new Processor();
+        final Executor codeToExecute = () -> System.out.println("bbb");
+        processor.execute(codeToExecute);*/
+
+        /*final Processor processor = new Processor();
+        processor.execute(() -> System.out.println("bbb"));*/
+
+        /*final ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+        System.out.println("Calculating expressions with lambdas");
+        expressionExecutor.executeExpression(10,5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10,5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10,5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10,5, (a, b) -> a / b);
+        expressionExecutor.executeExpression(10, 5, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(10, 5, FunctionalCalculator::subAFromB);
+        expressionExecutor.executeExpression(10, 5, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(10, 5, FunctionalCalculator::divideAByB);*/
+
+        /*final PoemBeautifier poemBeautifier = new PoemBeautifier();
 
         poemBeautifier.beautify("Text to Decorate", text -> text.toUpperCase());
         poemBeautifier.beautify("Text to Decorate", text -> "ABC" + text + "ABC");
@@ -20,5 +48,8 @@ public class StreamMain<text> {
                 return text + "- odd number of characters";
             }
         });
+    }*/
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
