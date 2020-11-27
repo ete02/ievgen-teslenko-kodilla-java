18d13c1cc3dce18e46279af52bb747a4274758d2
Ievgen Teslenko <eugen.te11@gmail.com>
2020-11-27 16:37:40 +0100

    correction module 7.1

--- kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
@@ -3,7 +3,7 @@ package com.kodilla.stream.beautifier;
 public class PoemBeautifier {
 
     public void beautify (String text, PoemDecorator poemDecorator) {
-        String result = poemDecorator.decorator(text);
+        String result = poemDecorator.decorate(text);
         System.out.println(result);
     }
 }
