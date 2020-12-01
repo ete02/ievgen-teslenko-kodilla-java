67279f7b8860af89d38786da4ef62d6cb81512c2
Ievgen Teslenko <eugen.te11@gmail.com>
2020-12-01 01:59:01 +0100

    Module 8.3 Exceptional Handling

---
+++ kodilla-exception/src/test/java/com/kodilla/exception/io/SecondChallengeTestSuite.java
@@ -0,0 +1,22 @@
+package com.kodilla.exception.io;
+
+import com.kodilla.exception.test.SecondChallenge;
+import com.kodilla.exception.test.ExceptionHandling;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class SecondChallengeTestSuite {
+
+    @Test
+    void secondChallengeTestTaskOne() {
+        //Given
+        SecondChallenge secondChallenge = new SecondChallenge();
+        //When & Then
+        assertAll(
+                () -> assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(2.0, 1.5)),
+                () -> assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(1.0, 1.5))
+        );
+
+    }
+}
