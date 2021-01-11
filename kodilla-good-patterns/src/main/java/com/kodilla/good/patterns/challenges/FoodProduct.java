a4003376fd4c2159579f63c8632ec6a48e925dd0
Ievgen Teslenko <eugen.te11@gmail.com>
2021-01-11 12:51:08 +0100

    Module 13.4 Food2Door

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/FoodProduct.java
@@ -0,0 +1,36 @@
+package com.kodilla.good.patterns.challenges;
+import java.util.Objects;
+
+public class FoodProduct {
+    private final String productName;
+    private final int price;
+
+    public FoodProduct(String productName, int price) {
+        this.productName = productName;
+        this.price = price;
+    }
+
+    public String getName() {
+        return productName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        FoodProduct that = (FoodProduct) o;
+        return price == that.price &&
+                Objects.equals(productName, that.productName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productName, price);
+    }
+}
