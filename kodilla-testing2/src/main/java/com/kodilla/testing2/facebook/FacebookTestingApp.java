5b5f96626c3005808f874a28c9d901d358668833
Ievgen Teslenko <eugen.te11@gmail.com>
2021-06-10 21:08:47 +0200

    Create FacebookTestingApp. Kodilla task 30.2

---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -0,0 +1,38 @@
+package com.kodilla.testing2.facebook;
+
+import com.kodilla.testing2.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class FacebookTestingApp {
+    public static final String SEARCH_FIELD = "u_0_2";
+    public static final String XPATH_WAIT_FOR = "//select[1]";
+    public static final String XPATH_DAY = "//div[contains(@class, \"_5k_5\")]/span/span/select[1]";
+    public static final String XPATH_MONTH = "//div[contains(@class, \"_5k_5\")]/span/span/select[2]";
+    public static final String XPATH_YEAR = "//div[contains(@class, \"_5k_5\")]/span/span/select[3]";
+
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        driver.get("https://www.facebook.com/");
+
+        WebElement searchField = driver.findElement(By.id(SEARCH_FIELD));
+        searchField.click();
+        Thread.sleep(2000);
+
+        while (!driver.findElement(By.xpath(XPATH_WAIT_FOR)).isDisplayed()) ;
+
+        WebElement selectComboDay = driver.findElement(By.xpath(XPATH_DAY));
+        Select selectDay = new Select(selectComboDay);
+        selectDay.selectByIndex(8);
+
+        WebElement selectComboMonth = driver.findElement(By.xpath(XPATH_MONTH));
+        Select selectMonth = new Select(selectComboMonth);
+        selectMonth.selectByIndex(3);
+
+        WebElement selectComboYear = driver.findElement(By.xpath(XPATH_YEAR));
+        Select selectYear = new Select(selectComboYear);
+        selectYear.selectByIndex(30);
+    }
+}
